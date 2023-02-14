0d9974daae3b929dac2be0d084c548eaa11ee61d
eskiimo <kareeemgm@gmail.com>
2023-02-14 19:27:44 +0200

    tailwind + insta layouts

--- components/posts/posts-list.js
+++ components/posts/posts-list.js
@@ -1,17 +1,41 @@
-import PostItem from "./post_item";
+import Link from "next/link";
 
 function PostsList(props) {
   return (
     <div>
       {props.posts.map((post) => {
         return (
-          <PostItem
-            id={post.id}
-            title={post.title}
-            image={post.image}
-            date={post.date}
-            location={post.location}
-          />
+          <div className="flex flex-col mx-auto w-5/6 sm:w-[40%] aspect-square	">
+            <div>
+              <div className="flex flex-row my-2 justify-start items-center	">
+                <div className="w-[35px] aspect-square mr-2 rounded-full bg-black border-2 border-pink-700"></div>
+                <div className="flex flex-col">
+                  <h3 className="font-bold text-white">{post.title}</h3>
+                  <p className="text-gray-300 text-sm ">
+                    {post.location.replace(", ", "\n")}
+                  </p>
+                </div>
+              </div>
+              <img src={"/" + post.image} alt={post.title} />
+              <div>
+                <h3 className="text-white my-1">
+                  {new Date(post.date).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </h3>
+              </div>
+
+              <div></div>
+            </div>
+            <div>
+              <Link href={`/${post.id}`}>
+                {" "}
+                <h1 className=" text-gray-500  ">Add a comment...</h1>
+              </Link>
+            </div>
+          </div>
         );
       })}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import PostsList from "../components/posts/posts-list";
-import { getFeaturedEvents } from "@/dummy-data";
+import { getAllEvents } from "@/dummy-data";
 
 function Home() {
-  const posts = getFeaturedEvents();
+  const posts = getAllEvents();
   return (
-    <>
-      <PostsList posts={posts} />
-    </>
+    <div className="flex flex-row min-w-[600px] xs:h-[100%] sm:h-[100vh]">
+      <div className="nav bg-black d-none sm:d-flex  sm:w-[150px] md:w-[350px] h-screen border-r border-1 border-gray-700"></div>
+      <div className="d-flex fixed sm:hidden  h-[60px] bottom-0 w-full bg-black border-1 border-t border-gray-700 p-2 text-white">
+        nav
+      </div>
+      <div className="h-[100%] w-full bg-black flex justify-center mx-auto overflow-y-scroll">
+        <PostsList posts={posts} />
+      </div>
+    </div>
   );
 }
 
