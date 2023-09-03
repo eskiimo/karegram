9f7cb3aacc0dd1fb8667d74e79456cf7d1ecb962
eskiimo <kareeemgm@gmail.com>
2023-09-04 00:00:16 +0300

    upload responsive

--- pages/create.js
+++ pages/create.js
@@ -55,23 +55,23 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="w-full h-[100vh] py-[7vh] dark:bg-black bg-slate-100   sm:w-[75vw]  flex justify-center md:items-center ">
+    <div className="w-full h-[100vh] mb-[7vh] py-[7vh] dark:bg-black bg-slate-100   sm:w-[75vw]  flex justify-center md:items-center  overflow-scroll">
       <form
         className="w-full md:w-[80%] h-[70%] flex flex-col "
         onSubmit={handleSubmit}
       >
         <div className="w-full h-full flex flex-col   justify-start  ">
           <ImageUpload shape="square" onInput={handleImage} />
-          <div className="flex flex-row items-center justify-start md:mb-[5%] md:w-full">
+          <div className="flex flex-row items-center justify-center px-1 md:mb-[5%]  w-full">
             <input
-              className=" w-5/6 p-3 my-3 ml-0 border-[1px] rounded-md dark:bg-black dark:text-white"
+              className=" w-4/6 p-3 my-3 ml-0 border-[1px] rounded-md dark:bg-black dark:text-white"
               type="text"
               ref={captionRef}
               placeholder="write a caption..."
             ></input>
             {!isLoading ? (
               <button
-                className=" w-1/6 py-3 m-3 px-5 mx-1 float-right rounded-lg  bg-blue-500 text-white "
+                className=" w-2/6 py-3 m-3 px-5 mx-1 float-right rounded-lg  bg-blue-500 text-white "
                 type="submit"
               >
                 Upload
